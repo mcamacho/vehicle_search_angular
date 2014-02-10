6a25b2ce347bef0eaadfad8ab781ee153be3d781
Mauricio Camacho <maurocamacho@gmail.com>
2014-02-10 17:59:57 -0500

    filters module - fulltext, dash.location

--- search-angular.js
+++ search-angular.js
@@ -1,3 +1,24 @@
+/*global angular*/
+'use strict';
+angular.module('filters', [])
+  .filter('fulltext', ['$filter', function($filter) {
+    return function(items, text) {
+      if (!text || text.length === 0)
+      return items;
+
+      // split search text on space
+      var searchTerms = text.split(' ');
+
+      // search for single terms.
+      // this reduces the item list step by step
+      searchTerms.forEach(function(term) {
+        if (term && term.length)
+          items = $filter('filter')(items, term);
+      });
+
+      return items;
+    };
+  }]);
 angular.module('services', [])
   .factory('dbVehicleValues', ['dash', function dbVehValFactory(dash) {
       return function(vehicleQuery) {
@@ -6,7 +27,7 @@ angular.module('services', [])
             var self = this;
             vehicleQuery.fetchFunction(function(items) {
                 self.items = items;
-                self.categories = dash.keys({ name: "m", last: "c" });
+                self.categories = dash.keys({ name: 'm', last: 'c' });
               });
           }
         };
@@ -14,7 +35,7 @@ angular.module('services', [])
         return loader;
       };
     }])
-  .factory('dash', function dashFactory() {
+  .factory('dash', ['$location', function dashFactory($location) {
     return {
       keys: function(obj) {
         var keyarray = [];
@@ -22,11 +43,12 @@ angular.module('services', [])
           this.push(key);
         }, keyarray);
         return keyarray;
-      }
+      },
+      dlocation: $location
     };
-  });
+  }]);
 
-var app = angular.module('project', ['services']);
+var app = angular.module('project', ['services', 'filters']);
 app.controller('GenCtrl', ['$scope', '$http', 'dbVehicleValues', 'dash', function($scope, $http, dbVehicleValues, dash) {
   var vehicleKeyQueryV1 = {
       year: { menuorder: 5 },
@@ -37,7 +59,7 @@ app.controller('GenCtrl', ['$scope', '$http', 'dbVehicleValues', 'dash', functio
         menuorder: 20,
         viewas: 'body'
       },
-      ext_color_code: {
+      exterior_color: {
         menuorder: 25,
         viewas: 'color'
       },
@@ -46,6 +68,7 @@ app.controller('GenCtrl', ['$scope', '$http', 'dbVehicleValues', 'dash', functio
       mileage: { slideorder: 10 },
       vin: {},
       image_1: {},
+      ext_color_code: {},
       evox_id: {}
     };
   var vehiclesKeys = dash.keys(vehicleKeyQueryV1);
@@ -56,7 +79,7 @@ app.controller('GenCtrl', ['$scope', '$http', 'dbVehicleValues', 'dash', functio
     k: vehiclesKeys.toString()
   };
   var fetchFunction = function(callback) {
-    $http.get('{home}/api/?', {
+    $http.get(dash.dlocation.protocol() + '://' + dash.dlocation.host() + '/api/?', {
       params: callParams
     })
       .success(callback);
