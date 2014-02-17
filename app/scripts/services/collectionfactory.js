9192d1592622ab8c7b1901d62d81bb07aba41c58
Mauricio Camacho <maurocamacho@gmail.com>
2014-02-17 09:55:25 -0500

    return list and menu categories function

--- app/scripts/services/collectionfactory.js
+++ app/scripts/services/collectionfactory.js
@@ -1,17 +1,24 @@
 'use strict';
 
 angular.module('vehicleSearchAngularApp')
-  .factory('collectionFactory', function() {
-    return function(vehicleQuery) {
-      var loader = {
-        _load: function() {
-          var self = this;
-          vehicleQuery.fetchFunction(function(items) {
-              self.items = items;
+  .factory('collectionFactory', function(_) {
+    return function(mainData) {
+      var collection = {
+        list: mainData,
+        menucategories: function(obj) {
+          var menuObj = _.pick(obj, function(objVal) {
+            return _.has(objVal, 'menuorder');
+          });
+          var category = _.mapValues(menuObj, function(objVal, keyVal) {
+            var values = _.pluck(this.list, keyVal);
+            var countValues = _.countBy(values, function(val) {
+              return val;
             });
+            return countValues;
+          }, this);
+          return category;
         }
       };
-      loader._load();
-      return loader;
+      return collection;
     };
   });
