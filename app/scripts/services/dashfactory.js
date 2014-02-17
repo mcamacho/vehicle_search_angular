4e22757af5052072ed055dee32cea8c81bbd3265
Mauricio Camacho <maurocamacho@gmail.com>
2014-02-17 09:55:36 -0500

    sanitation

--- app/scripts/services/dashfactory.js
+++ app/scripts/services/dashfactory.js
@@ -3,20 +3,6 @@
 angular.module('vehicleSearchAngularApp')
   .factory('dashFactory', ['$location', function($location) {
     return {
-      keys: function(obj) {
-        var keyarray = [];
-        angular.forEach(obj, function(value, key) {
-          this.push(key);
-        }, keyarray);
-        return keyarray;
-      },
-      values: function(obj, obvalue) {
-        var valuearray = [];
-        angular.forEach(obj, function(value) {
-          this.push(value[obvalue]);
-        }, valuearray);
-        return valuearray;
-      },
       queryValues: function(obj, obvalue) {
         var valuearray = [];
         angular.forEach(obj, function(value, key) {
