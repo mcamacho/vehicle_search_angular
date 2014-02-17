e3304b9f33f03f846a25734076b77c6cd31d0be1
Mauricio Camacho <maurocamacho@gmail.com>
2014-02-17 17:25:56 -0500

    extend properties of collection object, sort by order property

--- app/scripts/services/collectionfactory.js
+++ app/scripts/services/collectionfactory.js
@@ -4,20 +4,28 @@ angular.module('vehicleSearchAngularApp')
   .factory('collectionFactory', function(_) {
     return function(mainData) {
       var collection = {
+        // caching whole list
         list: mainData,
+        // creating a collection of categories with the menu property
         menucategories: function(obj) {
           var menuObj = _.pick(obj, function(objVal) {
-            return _.has(objVal, 'menuorder');
+            return _.has(objVal, 'menu');
           });
           var category = _.mapValues(menuObj, function(objVal, keyVal) {
             var values = _.pluck(this.list, keyVal);
             var countValues = _.countBy(values, function(val) {
               return val;
             });
-            return countValues;
+            return {
+              valueKey: keyVal,
+              valueLabel: objVal.menu.valueLabel,
+              order: objVal.menu.order,
+              options: countValues
+            };
           }, this);
-          return category;
+          return _.sortBy(category, 'order');
         }
+
       };
       return collection;
     };
