02d5d39979688e2446efa1691eedc6949890fed1
Mauricio Camacho <maurocamacho@gmail.com>
2014-02-17 09:54:10 -0500

    sanitation

--- app/scripts/controllers/searchcategory.js
+++ app/scripts/controllers/searchcategory.js
@@ -1,17 +1,5 @@
 'use strict';
 
 angular.module('vehicleSearchAngularApp')
-  .controller('searchCategoryCtrl', function ($scope, sourceFactory, dashFactory) {
-    var vehicleKeys = dashFactory.queryValues(sourceFactory.vehicleKeyV1, 'keyval');
-
-    $scope.$watch('searchLoader.items', function(){
-      var collection = [];
-      angular.forEach($scope.searchLoader.items, function(ele) {
-        // console.log('make', ele.make);
-        collection.push({ make: ele.make });
-      });
-      console.log(collection);
-      $scope.collection = collection;
-    });
-    console.log('collection-', $scope.collection);
-  });
+  .controller('searchCategoryCtrl', function () {
+});
